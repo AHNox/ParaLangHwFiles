cf0d7055db0dae4b6c552026c9f614f2b3931abd
Adam Nguyen <ahnguyen12300@gmail.com>
2024-03-25 15:13:36 -0500

    Removed comments for lexer

--- HW2/lexerTest.cpp
+++ HW2/lexerTest.cpp
@@ -13,22 +13,42 @@ int main(int argc, char *argv[])
     {
         ifstream file(argv[1]); // Open the file for reading
         char ch;
-        vector<string> splitUpLines;
+        vector<string> tokenStringArray;
 
         if (file.is_open())
         {
             string tempString = "";
+            bool lineIsComment = false;
+            int commentLength = 0;
             while (file.get(ch))
             {
-                int charInt = ch;
-                if (charInt == 10)
-                {
-                    cout << tempString << endl;
-                        splitUpLines.push_back(tempString);
+                // Checks if the current value is a newline
+                if (ch == '\n' || ch == '\r')
+                { // If it is then we add the current temp string into the array
+                    if (tempString.length() > 0 && tempString.length() > commentLength)
+                    {
+                        cout << "String: '" << tempString << "' with length of " << tempString.length() << " with a commentLength of " << commentLength << endl;
+                        int endPosition = tempString.length() - commentLength;
+                        cout << "Sliced String: " << tempString.substr(0, endPosition) << endl;
+                        tokenStringArray.push_back(tempString.substr(0, endPosition));
+                        tempString = "";
+                    }
                     tempString = "";
+                    lineIsComment = false;
+                    commentLength = 0;
                 }
                 else
-                {
+                { // If it isnt then we add the current char into the temp string
+                    // Checks if the character is a # which is the comment
+                    if (ch == '#')
+                    {
+                        lineIsComment = true;
+                    }
+                    // If this line is a part of the commnet, it adds 1 to the length
+                    if (lineIsComment == true)
+                    {
+                        commentLength += 1;
+                    }
                     tempString += ch;
                 }
             }
