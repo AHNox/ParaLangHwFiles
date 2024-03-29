4bb96a421d7fac19184938b8b1af28f40ae15d65
Adam Nguyen <101524918+AHNox@users.noreply.github.com>
2024-03-28 22:17:46 -0500

    Added token types

--- HW2/lexerTest.cpp
+++ HW2/lexerTest.cpp
@@ -3,6 +3,78 @@ using namespace std;
 #include <fstream>
 #include <vector>
 
+enum class TokenType
+{
+    // Single-character tokens
+    LEFT_PAREN,  // (         (0)
+    RIGHT_PAREN, // )         (1)
+    LEFT_BRACE,  // {         (2)
+    RIGHT_BRACE, // }         (3)
+    COMMA,       // ,         (4)
+    DOT,         // .         (5)
+    MINUS,       // -         (6)
+    PLUS,        // +         (7)
+    SEMICOLON,   // ;         (8)
+    SLASH,       // /         (9)
+    STAR,        // *         (10)
+
+    // One or two character tokens
+    BANG,          // !       (11)
+    BANG_EQUAL,    // !=      (12)
+    EQUAL,         // =       (13)
+    EQUAL_EQUAL,   // ==      (14)
+    GREATER,       // >       (15)
+    GREATER_EQUAL, // >=      (16)
+    LESS,          // <       (17)
+    LESS_EQUAL,    // <=      (18)
+
+    // Literals
+    IDENTIFIER, // identifier (19)
+    STRING,     // string     (20)
+    NUMBER,     // number     (21)
+
+    // Keywords
+    AND,      // and          (22)
+    AS,       // as           (23)
+    ASSERT,   // assert       (24)
+    BREAK,    // break        (25)
+    CLASS,    // class        (26)
+    CONTINUE, // continue     (27)
+    DEF,      // def          (28)
+    DEL,      // del          (29)
+    ELIF,     // elif         (30)
+    ELSE,     // else         (31)
+    EXCEPT,   // except       (32)
+    FALSE,    // False        (33)
+    FINALLY,  // finally      (34)
+    FOR,      // for          (35)
+    FROM,     // from         (36)
+    GLOBAL,   // global       (37)
+    IF,       // if           (38)
+    IMPORT,   // import       (39)
+    IN,       // in           (40)
+    IS,       // is           (41)
+    LAMBDA,   // lambda       (42)
+    NONE,     // None         (43)
+    NONLOCAL, // nonlocal     (44)
+    NOT,      // not          (45)
+    OR,       // or           (46)
+    PASS,     // pass         (47)
+    RAISE,    // raise        (48)
+    RETURN,   // return       (49)
+    TRUE,     // True         (50)
+    TRY,      // try          (51)
+    WHILE,    // while        (52)
+    WITH,     // with         (53)
+    YIELD,    // yield        (54)
+};
+
+struct token
+{
+    TokenType typeOfToken;
+    string contents;
+};
+
 int main(int argc, char *argv[])
 {
     vector<string> tokenStringVector;
@@ -88,6 +160,10 @@ int main(int argc, char *argv[])
     {
         cout << tokenStringVector[arrayIndex] << endl;
     }
+
+    TokenType tokenTypeAt1 = TokenType::STAR;
+
+    cout << "Token type: " << static_cast<int>(tokenTypeAt1) << endl;
 
     return 0;
 }
