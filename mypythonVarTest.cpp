41bdc84d6c22d5c59050324e6d54e83e1aab534c
Floyd Iglehart <145082420+The-IV@users.noreply.github.com>
2024-04-02 18:35:41 -0500

    Added comments

--- mypythonVarTest.cpp
+++ mypythonVarTest.cpp
@@ -7,19 +7,20 @@
 
 using namespace std;
 
-void Print(string line, unordered_map <string, string> variables)// Work in progress
+void Print(string line, unordered_map <string, string> variables)
 {
-    line = line.substr(6, line.size() - 7);
-    stringstream input(line);
-    string temp;
-    while(input.good())
+    line = line.substr(6);                      // Shaves down the line to not include "print(" and ")"
+    line.erase(line.find(')'));
+    stringstream input(line);                   // Declaration of stringstream to use passed lin as input
+    string temp;                                // Stand-in variable to help deal with inputs
+    while(input.good())                         // While loop runs until the stringstream is empty
     {
-        getline(input, temp, ',');
-        if (temp[0] == '"')
+        getline(input, temp, ',');              // Gets input up until the next comma
+        if (temp[0] == '"')                     // Prints everything surrounded by quotation marks as it is passed in
         {
             cout << temp.substr(1,temp.size() - 2);
         }
-        else
+        else                                    // Anything that is not in quotation marks is a variable for these testcases so we access the map and print its value
         {
             cout << variables[temp.substr(1,temp.size() -1)];
         }
@@ -30,6 +31,7 @@ void Print(string line, unordered_map <string, string> variables)// Work in prog
 
 bool isInt(string line)
 {
+    // Recursively checks every character in passed string to determine if it's a variable or an integer
     if (line.size() == 0)
         return true;
     else
@@ -43,6 +45,8 @@ bool isInt(string line)
 
 int operations(int a, int b, char oprt)
 {
+    //Includes all operations that are present in the testcases
+
     if (oprt == '+') {
         return a + b ;
     } 
@@ -61,31 +65,33 @@ int operations(int a, int b, char oprt)
 
 int EvalExpression(string line, unordered_map <string, string> variables)
 {
-    istringstream input(line);
-    string first;
-    char oprt;
-    string operand;
-    int temp;
-    int temp2;
-    stack <char> oprtStack;
-    stack <int> oprndStack;
+    istringstream input(line);  // Stringstream is used to utilize strings as inputs (rather than depending on console inputs)
+    string first;               // Variable for the first number in the expression
+    char oprt;                  // Variable for operators
+    string operand;             // Variable for operands (numbers)
+    int temp;                   // Stand-in varible for stack usage
+    int temp2;                  // Another stand-in
+    stack <char> oprtStack;     // Stack to keep track of operators. Used to establish precedence.
+    stack <int> oprndStack;     // Stack to keep track of operands. Used to establish precedence.
 
     input >> first;
-    if (isInt(first))
+    if (isInt(first))           // Checks if a variable is passed. If a variable is detected, then the map is accessed to find its value.
         temp = stoi(first);
     else
         temp = stoi(variables[first]);
 
-    oprndStack.push(temp);
+    oprndStack.push(temp);      // Pushes the first integer into the stack
+
+    //EXPRESSION EVALUATION BELOW
 
     while (input >> oprt >> operand)
     {
-        if (!isInt(operand))
+        if (!isInt(operand))                // Accesses value of variable if one is passed
             operand = variables[operand];
 
-        oprtStack.push(oprt);
+        oprtStack.push(oprt);               // Pushes next operator in the line to the operator stack
 
-        if (oprtStack.top() == '*' || oprtStack.top() == '/')
+        if (oprtStack.top() == '*' || oprtStack.top() == '/')   // If multiplication or division are detected, they are applied immediately because they have the highest precedence in these test cases
         {
             temp = oprndStack.top();
             oprndStack.pop();
@@ -95,11 +101,11 @@ int EvalExpression(string line, unordered_map <string, string> variables)
         }
         else 
         {
-            oprndStack.push(stoi(operand));
+            oprndStack.push(stoi(operand)); // If there's no multiplying or dividing, then we simply push the operand into the stack
         }
     }
 
-    while (oprndStack.size() != 1)
+    while (oprndStack.size() != 1)          // After all the multiplication and division has been done, we work out way through the stack and do out addition and subtraction
     {
             temp = oprndStack.top();
             oprndStack.pop();
@@ -109,7 +115,7 @@ int EvalExpression(string line, unordered_map <string, string> variables)
             oprtStack.pop();
     }
 
-    return oprndStack.top();
+    return oprndStack.top();                // The only value left in the stack by this point is the final value, so we return it
 }
 
 int main(int argc, char* argv[]) {
@@ -126,14 +132,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    unordered_map<string, string> variables; //used to store variables
+    unordered_map<string, string> variables; // Map used to store variable's names and assignments
 
     // Read and print the content of the file
     string line;
     while (getline(file, line)) {
-
-        //cout << line << endl;
-
         if (line.substr(0,6) == "print(") //If a print statement is detected, it runs our print function
         {
             Print(line, variables);
@@ -144,12 +147,10 @@ int main(int argc, char* argv[]) {
         {
             // If a variable declaration is detected, then the variable is added to our variable map
             int mid = line.find('=');
-            string name = line.substr(0,mid - 1);
-            string exp = line.substr(mid + 1);
-            //cout << exp << endl;
+            string name = line.substr(0,mid - 1); // Determines what the name of the variable is
+            string exp = line.substr(mid + 1); // Determines what the variable will be assigned
 
-            variables[name] = to_string(EvalExpression(exp, variables));
-            //cout << variables[name] << endl;
+            variables[name] = to_string(EvalExpression(exp, variables)); // Evaluates the right side of the variable declaration
         }
 
         else // Work in progress
